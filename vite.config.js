2e9442a0c5234a1df79034a1eb46a79e1eb78f6b
surajarya <surajarya745@gmail.com>
2023-12-08 22:03:26 +0530

    chek

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 export default {
+  optimizeDeps: {
+    include: ["jquery"],
+  },
   build: {
     rollupOptions: {
       input: {
-        index: "/main.js", // Common entry point for shared modules
-        homePage: "index.html", // Entry point for index.html
-        aboutPage: "/public/pages/about-us.html",
+        index: "./main.js", // Common entry point for shared modules
+        homePage: "./index.html", // Entry point for index.html
+        aboutPage: "/public/pages/about-us.html", // Entry point for about.html
         // blogPage: "/pages/blog.html",
         // contactPage: "/pages/contact.html",
         // galleryPage: "/pages/gallery.html",
