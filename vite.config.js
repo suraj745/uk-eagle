dbb920d00a5cbc571bd46fcd173fbd1d147a68fe
surajarya <surajarya745@gmail.com>
2023-12-08 22:01:41 +0530

    check 2

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,19 @@
 export default {
-  optimizeDeps: {
-    include: ["jquery"],
-  },
   build: {
     rollupOptions: {
       input: {
-        index: "main.js", // Common entry point for shared modules
+        index: "/main.js", // Common entry point for shared modules
         homePage: "index.html", // Entry point for index.html
-        aboutPage: "/pages/about.html", // Entry point for about.html
-        blogPage: "/pages/blog.html",
-        contactPage: "/pages/contact.html",
-        galleryPage: "/pages/gallery.html",
-        leadershipPage: "/pages/leadership.html",
-        mediaPage: "/pages/media_coverage.html",
-        TermsNconditions: "/pages/terms&condition.html",
-        privacypolicy: "/pages/privacy_policy.html",
-        eventPage: "/pages/events.html",
-        testimonials: "/pages/testimonials.html",
+        aboutPage: "/public/pages/about-us.html",
+        // blogPage: "/pages/blog.html",
+        // contactPage: "/pages/contact.html",
+        // galleryPage: "/pages/gallery.html",
+        // leadershipPage: "/pages/leadership.html",
+        // mediaPage: "/pages/media_coverage.html",
+        // TermsNconditions: "/pages/terms&condition.html",
+        // privacypolicy: "/pages/privacy_policy.html",
+        // eventPage: "/pages/events.html",
+        // testimonials: "/pages/testimonials.html",
       },
     },
   },
