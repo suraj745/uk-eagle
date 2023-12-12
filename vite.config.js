ee19eb1667b6cd1e40c51183c872acf209202ba9
surajarya <surajarya745@gmail.com>
2023-12-12 12:49:40 +0530

    check

--- vite.config.js
+++ vite.config.js
@@ -9,20 +9,19 @@ export default {
   base: "/",
   build: {
     rollupOptions: {
-      root,
-      // input: {
-      //   index: "./main.js", // Common entry point for shared modules
-      //   homePage: "/index.html", // Entry point for index.html
-      //   aboutPage: "/pages/about-us.html", // Entry point for about.html
-      //   servicePage: "/pages/our-services.html",
-      //   contactPage: "/pages/contact-us.html",
-      //   agriculturePage: "/pages/our-businesses/agriculture.html",
-      //   energyPage: "/pages/our-businesses/energy.html",
-      //   infrastructurePage: "/pages/our-businesses/infrastructure.html",
-      //   logisticsPage: "/pages/our-businesses/logistics.html",
-      //   pharmaceuticalPage: "/pages/our-businesses/pharmaceutical.html",
-      //   supermarketsPage: "/pages/our-businesses/supermarkets.html",
-      // },
+      input: {
+        index: "./main.js", // Common entry point for shared modules
+        homePage: "/index.html", // Entry point for index.html
+        aboutPage: "/pages/about-us.html", // Entry point for about.html
+        servicePage: "/pages/our-services.html",
+        contactPage: "/pages/contact-us.html",
+        agriculturePage: "/pages/our-businesses/agriculture.html",
+        energyPage: "/pages/our-businesses/energy.html",
+        infrastructurePage: "/pages/our-businesses/infrastructure.html",
+        logisticsPage: "/pages/our-businesses/logistics.html",
+        pharmaceuticalPage: "/pages/our-businesses/pharmaceutical.html",
+        supermarketsPage: "/pages/our-businesses/supermarkets.html",
+      },
     },
   },
 };
