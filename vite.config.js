815f838290c135741cbb98cc6ddbf70b0a3da8e6
surajarya <surajarya745@gmail.com>
2023-12-12 12:40:26 +0530

    check

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,28 @@
+import { resolve } from "path";
+
+const root = resolve(__dirname, "src");
+
 export default {
   optimizeDeps: {
     include: ["jquery"],
   },
   base: "/",
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       index: "./main.js", // Common entry point for shared modules
-  //       homePage: "/index.html", // Entry point for index.html
-  //       aboutPage: "/pages/about-us.html", // Entry point for about.html
-  //       servicePage: "/pages/our-services.html",
-  //       contactPage: "/pages/contact-us.html",
-  //       agriculturePage: "/pages/our-businesses/agriculture.html",
-  //       energyPage: "/pages/our-businesses/energy.html",
-  //       infrastructurePage: "/pages/our-businesses/infrastructure.html",
-  //       logisticsPage: "/pages/our-businesses/logistics.html",
-  //       pharmaceuticalPage: "/pages/our-businesses/pharmaceutical.html",
-  //       supermarketsPage: "/pages/our-businesses/supermarkets.html",
-  //     },
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      root,
+      // input: {
+      //   index: "./main.js", // Common entry point for shared modules
+      //   homePage: "/index.html", // Entry point for index.html
+      //   aboutPage: "/pages/about-us.html", // Entry point for about.html
+      //   servicePage: "/pages/our-services.html",
+      //   contactPage: "/pages/contact-us.html",
+      //   agriculturePage: "/pages/our-businesses/agriculture.html",
+      //   energyPage: "/pages/our-businesses/energy.html",
+      //   infrastructurePage: "/pages/our-businesses/infrastructure.html",
+      //   logisticsPage: "/pages/our-businesses/logistics.html",
+      //   pharmaceuticalPage: "/pages/our-businesses/pharmaceutical.html",
+      //   supermarketsPage: "/pages/our-businesses/supermarkets.html",
+      // },
+    },
+  },
 };
